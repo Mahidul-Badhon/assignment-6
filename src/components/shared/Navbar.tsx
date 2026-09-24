e5ece8f35ef91d25b1505fb115d8bcb9c6ae94d6
Mahidul-Badhon <mahidul.badhon28@gmail.com>
2026-09-24 18:56:55 +0600

    Navbar added

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import React from 'react';
+import logo from "@/assets/logo.png"
+
+const Navbar = () => {
+    return (
+        <div className=' border-b border-slate-500'>
+            <div className="navbar bg-black container mx-auto shadow-sm">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg aria-label="Menu" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex={-1}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            <li><a>Workouts</a></li>
+                            <li><a>My plan</a></li>
+                        </ul>
+                    </div>
+                    
+                    <Image src={logo} alt='Logo'></Image>
+                    <a className="btn btn-ghost text-xl">FITLOG</a>
+                    
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a>Workouts</a></li>
+                        <li><a>My Plan</a></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <button className="btn">Saved</button>
+                    <button className="btn">Plan</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
