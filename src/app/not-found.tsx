2241e39e09d79d6c1086f36cf1d4776fcc20a2db
Mahidul-Badhon <mahidul.badhon28@gmail.com>
2026-09-26 20:17:34 +0600

    not found page added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Notfound = () => {
+    return (
+        <div>
+            <h2 className='font-extrabold text-6xl text-white'>404 NOt Found!!!</h2>
+        </div>
+    );
+};
+
+export default Notfound;
